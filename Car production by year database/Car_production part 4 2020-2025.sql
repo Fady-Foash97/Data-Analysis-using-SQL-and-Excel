c7f2f7a32f46a7d1a703c877470a6a45e69a234c
Fady Ashraf <fadysamweal8@gmail.com>
2026-01-26 23:21:25 +0200

    Add files via upload

---
+++ Car production by year database/Car_production part 4 2020-2025.sql
@@ -0,0 +1,108 @@
+CREATE TABLE Car_production_2020(
+    ID Int PRIMARY KEY,
+    Country VARCHAR(50),
+    CARS BIGINT,
+    Commercial BIGINT
+);
+INSERT INTO Car_production_2020
+VALUES 
+(1, "China", 21444743, 4676969),
+(2, "Japan", 6619245, 1217663),
+(3, "India", 3631095, 768017),
+(4, "South Korea", 3162727, 299677),
+(5, "Germany", 3096165, 0),
+(6, "Brazil", 1707851, 540402),
+(7, "Spain", 2448490, 496498),
+(8, "USA", 2248291, 574341),
+(9, "Russia", 1662963, 0),
+(10, "UK", 987044, 51244);
+CREATE TABLE Car_production_2021(
+    ID Int PRIMARY KEY,
+    Country VARCHAR(50),
+    CARS BIGINT,
+    Commercial BIGINT
+);
+INSERT INTO Car_production_2021
+VALUES 
+(1, "China", 26082220, 2171546),
+(2, "USA", 9167214, 7315903),
+(3, "Japan", 7846955, 708524),
+(4, "India", 4399112, 486911),
+(5, "South Korea", 3462404, 227673),
+(6, "Germany", 3308692, 212527),
+(7, "Brazil", 2248253, 362711),
+(8, "Spain", 2098133, 383736),
+(9, "Russia", 1566317, 130936),
+(10, "France", 1351308, 433401);
+CREATE TABLE Car_production_2022(
+    ID Int PRIMARY KEY,
+    Country VARCHAR(50),
+    CARS BIGINT,
+    Commercial BIGINT
+);
+INSERT INTO Car_production_2022
+VALUES 
+(1, "China", 23836083, 3184532),
+(2, "Japan", 6566356, 1269183),
+(3, "India", 4439144, 1018098),
+(4, "Germany", 3480357, 0),
+(5, "South Korea", 3438355, 318694),
+(6, "Brazil", 1824833, 544936),
+(7, "Spain", 1787197, 432239),
+(8, "USA", 1703608, 8349350),
+(9, "France", 1383173, 372707),
+(10, "Russia", 608460, 83813);
+CREATE TABLE Car_production_2023(
+    ID Int PRIMARY KEY,
+    Country VARCHAR(50),
+    CARS BIGINT,
+    Commercial BIGINT
+);
+INSERT INTO Car_production_2023
+VALUES 
+(1, "China", 26123757, 4037209),
+(2, "Japan", 7767058, 1231480),
+(3, "India", 4783628, 1068515),
+(4, "Germany", 4109371, 0),
+(5, "South Korea", 3908747, 334850),
+(6, "Spain", 1907072, 544171),
+(7, "Brazil", 1782079, 542759),
+(8, "USA", 1741083, 8898057),
+(9, "France", 1505076, 478386),
+(10, "UK", 1025474, 97784);
+CREATE TABLE Car_production_2024(
+    ID Int PRIMARY KEY,
+    Country VARCHAR(50),
+    CARS BIGINT,
+    Commercial BIGINT
+);
+INSERT INTO Car_production_2024
+VALUES 
+(1, "China", 27476886, 3804706),
+(2, "Japan", 7139188, 1095493),
+(3, "India", 4991413, 1023278),
+(4, "Germany", 4069222, 0),
+(5, "South Korea", 3849326, 277926),
+(6, "Spain", 1918244, 458260),
+(7, "Brazil", 1895020, 654575),
+(8, "USA", 1741083, 8898057),
+(9, "Indonesia", 1026976, 101237),
+(10, "Russia", 982665, 125908);
+CREATE TABLE Car_production_2025(
+    ID Int PRIMARY KEY,
+    Country VARCHAR(50),
+    CARS BIGINT,
+    Commercial BIGINT
+);
+INSERT INTO Car_production_2025
+VALUES 
+(1, "China", 21241210, 3091486),
+(2, "Japan", 5329880, 889270),
+(3, "India", 3981914, 820929),
+(4, "Germany", 3145440, 0),
+(5, "South Korea", 2881799, 200814),
+(6, "Brazil", 1494199, 492426),
+(7, "Spain", 1360130, 341920),
+(8, "USA", 981836, 6603925),
+(9, "France", 778329, 281060),
+(10, "Russia", 511702, 63653);
